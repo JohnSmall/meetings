1b18b664ff4a47b06038cfc618506440eef76a7d
John Small <john.small@bbc.com>
2017-01-15 21:48:15 +0000

    removed unneeded specs and got the ones we do need working

--- app/models/influence_office_person.rb
+++ app/models/influence_office_person.rb
@@ -6,10 +6,9 @@ class InfluenceOfficePerson < ApplicationRecord
     scope klass.underscore.downcase.pluralize.to_sym,->{where(type: klass)}
   end
   validates :means_of_influence_id,presence: true
-  belongs_to :meeting, foreign_key: 'means_of_influence_id' 
-  # We might need to change these to scopes as the factory specs report errors because the records are invalid
-  # belongs_to :hospitality, foreign_key: 'means_of_influence_id'
-  # belongs_to :gift, foreign_key: 'means_of_influence_id'
-  # belongs_to :travel, foreign_key: 'means_of_influence_id'
+  belongs_to :meeting, foreign_key: 'means_of_influence_id',optional: true 
+  belongs_to :hospitality, foreign_key: 'means_of_influence_id',optional: true
+  belongs_to :gift, foreign_key: 'means_of_influence_id',optional: true
+  belongs_to :travel, foreign_key: 'means_of_influence_id',optional: true
   belongs_to :person
 end
